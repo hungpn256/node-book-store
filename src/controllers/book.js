40acf06bfb9f130700e89a4805fdfbc1a00f1023
Hung Pham <phamnanghung.25@gmail.com>
2021-11-08 09:05:08 +0700

    get boook

--- src/controllers/book.js
+++ src/controllers/book.js
@@ -1,41 +1,25 @@
 const BookItem = require('../models/bookItem.js')
-exports.getAll = (req, res) => {
+exports.getAll = async (req, res) => {
     try {
         // Validate request
-        BookItem.getAll((err, data) => {
-            if (err)
-                res.status(500).send({
-                    message:
-                        err.message || "Some error occurred while get all book"
-                });
-            else {
-                res.send(data)
-            }
-        })
+        const res = await BookItem.getAll();
+        res.send(res)
     }
     catch (err) {
         console.log('err', err);
-        res.send(err)
+        res.status(400).send(err)
     }
 
 };
-exports.getItemById = (req, res) => {
+exports.getItemById = async (req, res) => {
     try {
         const id = parseInt(req.params.id)
-        BookItem.getById(id, (err, data) => {
-            if (err)
-                res.status(500).send({
-                    message:
-                        err.message || "Some error occurred while get all book"
-                });
-            else {
-                res.send(data)
-            }
-        })
+        const data = await BookItem.getById(id)
+        res.send(data)
     }
     catch (err) {
         console.log('err', err);
-        res.send(err)
+        res.status(400).send(err)
     }
 
 };
